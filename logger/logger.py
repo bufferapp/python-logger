5fee5f972b900df94ebc54e7aa05dd8b093dfc89
Steven Cheng <stevenc81@gmail.com>
2016-09-17 15:36:37 +0200

    Added metrics logging

--- logger/logger.py
+++ logger/logger.py
@@ -16,14 +16,29 @@ class ContextFilter(logging.Filter):
         return True
 
 def getLogger(svc_name='test', level=logging.INFO):
-    logger = logging.getLogger('k8s_logger')
+    logger = logging.getLogger('k8s_logger_' + svc_name)
     logger.setLevel(level)
 
     f = ContextFilter(svc_name)
     logger.addFilter(f)
 
     handler = StreamHandler(sys.stdout)
-    formatter = logging.Formatter('{"name": "%(svc_name)s", "msg": "%(message)s", "timestamp": "%(asctime)s", "hostname": "%(hostname)s"}', datefmt='%Y-%m-%dT%H:%M:%S.000Z')
+    formatter = logging.Formatter('{"name": "%(svc_name)s", "msg": "%(message)s", "timestamp": "%(asctime)s.%(msecs)03dZ", "hostname": "%(hostname)s"}', datefmt='%Y-%m-%dT%H:%M:%S')
+
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+def getTracker(svc_name='test', level=logging.INFO):
+    logger = logging.getLogger('k8s_metrics_tracker_' + svc_name)
+    logger.setLevel(level)
+
+    f = ContextFilter(svc_name)
+    logger.addFilter(f)
+
+    handler = StreamHandler(sys.stdout)
+    formatter = logging.Formatter('{"type": "%(svc_name)s", "value": "%(message)s", "date": "%(asctime)s.%(msecs)03dZ", "hostname": "%(hostname)s", "buffermetrics": true}', datefmt='%Y-%m-%dT%H:%M:%S')
 
     handler.setFormatter(formatter)
     logger.addHandler(handler)
