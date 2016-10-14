7274e104f8621cfe3a15831d590ebcdf01339c2f
Steven Cheng <stevenc81@gmail.com>
2016-10-14 17:48:09 +0800

    Added fluentd forwarder to one of the logging options

--- logger/logger.py
+++ logger/logger.py
@@ -2,6 +2,23 @@ import logging
 import socket
 from logging import StreamHandler
 import sys
+from fluent import handler as fluent_handler
+import json
+
+log_custom_format = {
+    "name": "%(svc_name)s",
+    "msg": "%(message)s",
+    "timestamp": "%(asctime)s.%(msecs)03dZ",
+    "hostname": "%(hostname)s"
+}
+
+metric_custom_format = {
+    "type": "%(svc_name)s",
+    "value": "%(message)s",
+    "date": "%(asctime)s.%(msecs)03dZ",
+    "hostname": "%(hostname)s",
+    "buffermetrics": "true"
+}
 
 class ContextFilter(logging.Filter):
     hostname = socket.gethostname()
@@ -15,30 +32,38 @@ class ContextFilter(logging.Filter):
         record.svc_name = self.svc_name
         return True
 
-def getLogger(svc_name='test', level=logging.INFO):
+def getLogger(svc_name='test', output='stdout', level=logging.INFO):
     logger = logging.getLogger('k8s_logger_' + svc_name)
     logger.setLevel(level)
 
     f = ContextFilter(svc_name)
     logger.addFilter(f)
 
-    handler = StreamHandler(sys.stdout)
-    formatter = logging.Formatter('{"name": "%(svc_name)s", "msg": "%(message)s", "timestamp": "%(asctime)s.%(msecs)03dZ", "hostname": "%(hostname)s"}', datefmt='%Y-%m-%dT%H:%M:%S')
+    if output == 'td-agent-forward':
+        handler = fluent_handler.FluentHandler('application.logs', host='log-aggregator-service.default', port=24224)
+        formatter = fluent_handler.FluentRecordFormatter(log_custom_format, datefmt='%Y-%m-%dT%H:%M:%S')
+    else:
+        handler = StreamHandler(sys.stdout)
+        formatter = logging.Formatter(json.dumps(log_custom_format), datefmt='%Y-%m-%dT%H:%M:%S')
 
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
     return logger
 
-def getTracker(svc_name='test', level=logging.INFO):
+def getTracker(svc_name='test', output='stdout', level=logging.INFO):
     logger = logging.getLogger('k8s_metrics_tracker_' + svc_name)
     logger.setLevel(level)
 
     f = ContextFilter(svc_name)
     logger.addFilter(f)
 
-    handler = StreamHandler(sys.stdout)
-    formatter = logging.Formatter('{"type": "%(svc_name)s", "value": %(message)s, "date": "%(asctime)s.%(msecs)03dZ", "hostname": "%(hostname)s", "buffermetrics": true}', datefmt='%Y-%m-%dT%H:%M:%S')
+    if output == 'td-agent-forward':
+        handler = fluent_handler.FluentHandler('application.metrics', host='log-aggregator-service.default', port=24224)
+        formatter = fluent_handler.FluentRecordFormatter(metric_custom_format, datefmt='%Y-%m-%dT%H:%M:%S')
+    else:
+        handler = StreamHandler(sys.stdout)
+        formatter = logging.Formatter(json.dumps(metric_custom_format), datefmt='%Y-%m-%dT%H:%M:%S')
 
     handler.setFormatter(formatter)
     logger.addHandler(handler)
